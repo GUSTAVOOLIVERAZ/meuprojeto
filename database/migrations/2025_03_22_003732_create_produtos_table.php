0e3415dd7e05c8867de42cce68e402ededf2a683
Gustavo Oliveira <gustavooliveiracf3@gmail.com>
2025-03-22 22:05:01 -0300

    Update 2025_03_22_003732_create_produtos_table.php

--- database/migrations/2025_03_22_003732_create_produtos_table.php
+++ database/migrations/2025_03_22_003732_create_produtos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->integer('quantidade');
+            $table->float('valor');
             $table->timestamps();
         });
     }
